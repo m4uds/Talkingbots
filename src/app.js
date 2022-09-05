e9a7e7fa0f6c57f3f0872403159eba3e8fac93eb
m-a-u-d <maudfreeman@gmail.com>
2022-09-05 22:58:46 +1000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 function main(){
 
     setInterval(buildConvosation, 1000);
-    console.log("=)")
+    console.log("m4uds made this")
 
 }
 
@@ -17,9 +17,10 @@ async function buildConvosation (){
     var data = []
     await LoadConversation().then((Response => data = Response))
      console.log(data)
-    
+     $("#text-cont").html("")
     for (const element of data) {
         console.log()
+        
         $( "#text-cont" ).append( "<h1>"+Object.values(element)+"</h1>" );
       }
       
